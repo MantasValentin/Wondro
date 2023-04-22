c37cab3c4000ad32778d58e4e930fb305b40349b
MantasValentin <mantasvalentin@protonmail.com>
2023-04-22 18:08:29 +0300

    link routing fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,22 +5,22 @@ const Footer: React.FC = () => {
   return (
     <footer className="flex items-center justify-center bg-black text-white w-full h-20">
       <ul className="flex flex-row flex-wrap items-center justify-center gap-4">
-        <Link href="./" className="transition opacity-70 hover:opacity-100">
+        <Link href="/" className="transition opacity-70 hover:opacity-100">
           About
         </Link>
-        <Link href="./" className="transition opacity-70 hover:opacity-100">
+        <Link href="/" className="transition opacity-70 hover:opacity-100">
           Blog
         </Link>
-        <Link href="./" className="transition opacity-70 hover:opacity-100">
+        <Link href="/" className="transition opacity-70 hover:opacity-100">
           News
         </Link>
-        <Link href="./" className="transition opacity-70 hover:opacity-100">
+        <Link href="/" className="transition opacity-70 hover:opacity-100">
           Careers
         </Link>
-        <Link href="./" className="transition opacity-70 hover:opacity-100">
+        <Link href="/" className="transition opacity-70 hover:opacity-100">
           FAQs
         </Link>
-        <Link href="./" className="transition opacity-70 hover:opacity-100">
+        <Link href="/" className="transition opacity-70 hover:opacity-100">
           Contact Us
         </Link>
       </ul>
