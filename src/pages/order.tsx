75293abc3fcf76d66fef5b4594398e8f92f1f98a
MantasValentin <mantasvalentin@protonmail.com>
2023-04-22 04:19:55 +0300

    test

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -65,106 +65,112 @@ const Order: React.FC = () => {
   return (
     <Layout>
       <main className="flex flex-col min-h-full h-fit px-40 py-6 max-xl:p-6 gap-8">
-        <div className="text-2xl font-semibold">Place Order</div>
-        <div className="flex flex-row max-md:flex-col gap-8">
-          <div className="flex flex-1 flex-col h-fit bg-gray-200 px-8 py-4 gap-8">
-            <div className="flex flex-col gap-2">
-              <div className="flex items-center text-xl font-semibold">
-                Shipping Address
-              </div>
-              <div className="text-lg">
-                {state.cart.shippingAddress.fullName},{" "}
-                {state.cart.shippingAddress.address},{" "}
-                {state.cart.shippingAddress.city},{" "}
-                {state.cart.shippingAddress.postalCode},{" "}
-                {state.cart.shippingAddress.country}
-              </div>
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="flex items-center text-xl font-semibold">
-                Payment Method
-              </div>
-              <div>{state.cart.paymentMethod}</div>
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="flex items-center text-xl font-semibold">
-                Order Items
-              </div>
-              <div className="flex flex-row text-xl max-sm:text-lg font-semibold">
-                <div className="flex items-center basis-2/5">Item</div>
-                <div className="flex items-center justify-center basis-1/5">
-                  Quantity
+        {state.cart.cartItems.length ? (
+          <>
+            <div className="text-2xl font-semibold">Place Order</div>
+            <div className="flex flex-row max-md:flex-col gap-8">
+              <div className="flex flex-1 flex-col h-fit bg-gray-200 px-8 py-4 gap-8">
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-center text-xl font-semibold">
+                    Shipping Address
+                  </div>
+                  <div className="text-lg">
+                    {state.cart.shippingAddress.fullName},{" "}
+                    {state.cart.shippingAddress.address},{" "}
+                    {state.cart.shippingAddress.city},{" "}
+                    {state.cart.shippingAddress.postalCode},{" "}
+                    {state.cart.shippingAddress.country}
+                  </div>
                 </div>
-                <div className="flex items-center justify-center basis-1/5">
-                  Price
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-center text-xl font-semibold">
+                    Payment Method
+                  </div>
+                  <div>{state.cart.paymentMethod}</div>
                 </div>
-                <div className="flex items-center justify-center basis-1/5">
-                  Subtotal
-                </div>
-              </div>
-              {state.cart.cartItems.map((item: Item) => {
-                return (
-                  <div
-                    className="flex flex-row text-xl max-sm:text-base"
-                    key={item.purchaseId}
-                  >
-                    <div className="flex items-center basis-2/5 gap-4">
-                      <div className="relative max-w-[4rem] border-[1px] border-black border-opacity-30 transition duration-300 hover:border-opacity-70 max-lg:hidden">
-                        <Image
-                          src={item.img.src}
-                          alt={item.description}
-                          width={item.img.width}
-                          height={item.img.height}
-                          className="w-full h-full object-cover bg-white"
-                        ></Image>
-                      </div>
-                      <div className="">{item.description}</div>
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-center text-xl font-semibold">
+                    Order Items
+                  </div>
+                  <div className="flex flex-row text-xl max-sm:text-lg font-semibold">
+                    <div className="flex items-center basis-2/5">Item</div>
+                    <div className="flex items-center justify-center basis-1/5">
+                      Quantity
                     </div>
                     <div className="flex items-center justify-center basis-1/5">
-                      {item.quantity}
+                      Price
                     </div>
                     <div className="flex items-center justify-center basis-1/5">
-                      {item.price}.00&euro;
-                    </div>
-                    <div className="flex items-center justify-center basis-1/5">
-                      {item.quantity * item.price}.00&euro;
+                      Subtotal
                     </div>
                   </div>
-                );
-              })}
-            </div>
-          </div>
-          <div className="flex flex-col h-fit min-w-[20rem] bg-gray-200 px-8 py-4 gap-4">
-            <div className="flex justify-center items-center text-2xl font-semibold">
-              Order Summary
-            </div>
-            <div className="flex justify-between text-xl">
-              <div>Items</div>
-              <div>{itemPrice}.00&euro;</div>
-            </div>
-            <div className="flex justify-between text-xl">
-              <div>Tax</div>
-              <div>{Math.round(itemPrice * 0.15)}.00&euro;</div>
-            </div>
-            <div className="flex justify-between text-xl">
-              <div>Shipping</div>
-              <div>{itemPrice > 200 ? 0 : 15}.00&euro;</div>
-            </div>
-            <div className="flex justify-between text-xl">
-              <div>Total</div>
-              <div>
-                {totalPrice}
-                .00&euro;
+                  {state.cart.cartItems.map((item: Item) => {
+                    return (
+                      <div
+                        className="flex flex-row text-xl max-sm:text-base"
+                        key={item.purchaseId}
+                      >
+                        <div className="flex items-center basis-2/5 gap-4">
+                          <div className="relative max-w-[4rem] border-[1px] border-black border-opacity-30 transition duration-300 hover:border-opacity-70 max-lg:hidden">
+                            <Image
+                              src={item.img.src}
+                              alt={item.description}
+                              width={item.img.width}
+                              height={item.img.height}
+                              className="w-full h-full object-cover bg-white"
+                            ></Image>
+                          </div>
+                          <div className="">{item.description}</div>
+                        </div>
+                        <div className="flex items-center justify-center basis-1/5">
+                          {item.quantity}
+                        </div>
+                        <div className="flex items-center justify-center basis-1/5">
+                          {item.price}.00&euro;
+                        </div>
+                        <div className="flex items-center justify-center basis-1/5">
+                          {item.quantity * item.price}.00&euro;
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="flex flex-col h-fit min-w-[20rem] bg-gray-200 px-8 py-4 gap-4">
+                <div className="flex justify-center items-center text-2xl font-semibold">
+                  Order Summary
+                </div>
+                <div className="flex justify-between text-xl">
+                  <div>Items</div>
+                  <div>{itemPrice}.00&euro;</div>
+                </div>
+                <div className="flex justify-between text-xl">
+                  <div>Tax</div>
+                  <div>{Math.round(itemPrice * 0.15)}.00&euro;</div>
+                </div>
+                <div className="flex justify-between text-xl">
+                  <div>Shipping</div>
+                  <div>{itemPrice > 200 ? 0 : 15}.00&euro;</div>
+                </div>
+                <div className="flex justify-between text-xl">
+                  <div>Total</div>
+                  <div>
+                    {totalPrice}
+                    .00&euro;
+                  </div>
+                </div>
+                <button
+                  className="text-xl text-center px-4 py-2 border-[1px] border-black bg-gray-900 text-white font-semibold hover:cursor-pointer hover:bg-white hover:text-black active:scale-90 transition select-none"
+                  onClick={placeOrder}
+                >
+                  Place Order
+                </button>
               </div>
             </div>
-            <button
-              className="text-xl text-center px-4 py-2 border-[1px] border-black bg-gray-900 text-white font-semibold hover:cursor-pointer hover:bg-white hover:text-black active:scale-90 transition select-none"
-              onClick={placeOrder}
-            >
-              Place Order
-            </button>
-          </div>
-        </div>
+          </>
+        ) : (
+          <></>
+        )}
       </main>
     </Layout>
   );
